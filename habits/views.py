a9b6c0182f2b358d6a760a48bbc05fa1f224d84b
Alexandr Zibinin <zibinin.chel@gmail.com>
2024-08-21 02:18:49 +0300

    create paginations

--- habits/views.py
+++ habits/views.py
@@ -2,11 +2,19 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 
 from habits.models import Habit
+from habits.pagination import CustomHabitPagination
 from habits.serializer import HabitSerializer
 
 
 class HabitListAPIView(ListAPIView):
     serializer_class = HabitSerializer
+    pagination_class = CustomHabitPagination
+
+    def get(self, request):
+        queryset = Habit.objects.all()
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = HabitSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
 
 class HabitCreateAPIView(CreateAPIView):
     serializer_class = HabitSerializer
